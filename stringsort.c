f193701b6eddc4afa1a1a568279c5b77b54de44c
Rsl78 <rasel@graduate.utm.my>
2023-12-25 13:20:44 +0800

    string counting and sorting

---
+++ stringsort.c
@@ -0,0 +1,36 @@
+/*
+1. take input
+2. find the largest element from the string
+3. Diclear a new array with highest+1 size
+4. Assign 0 to all value in highestSizeArray
+5. find the value of a string (str[i]-'a')+1
+
+*/ 
+
+#include <stdio.h>
+
+int main()
+{
+    // Step 1: take array size input
+  
+
+    // Step 2: Take array input
+
+
+    // Step 3: Find the highest element
+
+
+    // Step 4: Diclear a new array with highest+1 size
+
+
+    // Step 5: Assign 0 to all value in highestSizeArray
+
+
+    // Step 6: find the available value and assign 1 to the highestSizeArray
+
+
+    // step 7: Print sorted array
+
+
+    return 0;
+}
