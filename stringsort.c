d4c0db919bb30b6b1bee779eb7d301b48be36c17
Rsl78 <rasel@graduate.utm.my>
2023-12-25 13:22:32 +0800

    string sorting and counting

--- stringsort.c
+++ stringsort.c
@@ -1,9 +1,12 @@
 /*
 1. take input
-2. find the largest element from the string
-3. Diclear a new array with highest+1 size
-4. Assign 0 to all value in highestSizeArray
-5. find the value of a string (str[i]-'a')+1
+2. Find the length o string
+3. find the largest element from the string
+4. Diclear a new array with highest+1 size
+5. Assign 0 to all value in highestSizeArray
+6. find the value of a string (str[i]-'a')+1
+7. 
+8. every time next largest value and the corresponding character  filter 
 
 */ 
 
@@ -11,26 +14,65 @@
 
 int main()
 {
-    // Step 1: take array size input
-  
+    // Step 1: take input
+    char str[1000];
+    scanf("%s", str);
 
-    // Step 2: Take array input
+    // Step 2: find the length of input
+    int i=0,n=0;
 
+    while(str[i]!='\0'){
+        i++;
+        n++;
+    }
 
     // Step 3: Find the highest element
-
+    int largestElement= 26; 
 
     // Step 4: Diclear a new array with highest+1 size
-
+    int freq[largestElement+1];
 
     // Step 5: Assign 0 to all value in highestSizeArray
+    for(int i=1;i<=largestElement;i++){
+        freq[i]=0;
+    }
 
+    // Step 6: find the available value and increment to the highestSizeArray
+    for(int i=0;i<n;i++){
+        freq[(str[i]-'a')+1]++;
+    }
+    // step 7: total iteration 
+    int total=0;
+    for(int i=1;i<= largestElement;i++){
+        if(freq[i]>0){
+            total++;
+        }
+    }
 
-    // Step 6: find the available value and assign 1 to the highestSizeArray
+    // step 8: Print sorted array
+    int nxt =0;
+    while(nxt!=total){
+        int max=-1,index=-1;
+        char ch;
 
+        for (int i = 1; i < largestElement; i++)
+        {
+            if (freq[i]>max){
+                max = freq[i];
+                ch= i+96;
+                index=i;
+            }  
+        }
+        // printf("Next greatest value - %d \n", max);
+        // printf("Character - %c\n\n", ch);
 
-    // step 7: Print sorted array
+        for(int i=1;i<=max;i++){
+            printf("%c",ch);
+        }
 
+        freq[index]=0;
+        nxt++;
+    }
 
     return 0;
 }
